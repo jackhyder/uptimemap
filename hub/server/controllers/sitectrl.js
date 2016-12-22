0bc9e73f9cdbafd6fe989ba257ca1633d25dad01
Shirfeng Chang <shirfeng.chang@gmail.com>
2016-12-21 20:48:47 -0800

    snapshot

--- hub/server/controllers/sitectrl.js
+++ hub/server/controllers/sitectrl.js
@@ -1,5 +1,23 @@
-var mongoose = require('mongoose');
-var site = mongoose.model('Site')
+var mongoose = require('mongoose'), path = require('path'), datalogic = require(path.join(__dirname,'datalogic.js'));
+var site = mongoose.model('Site');
+var data = datalogic.data;
+var count = 0;
+setInterval(function(){
+  count +=1;
+  console.log("########" + count);
+  console.log(
+    data.sort(function(a,b){
+      if (a.nxtchk.getTime() > b.nxtchk.getTime()){
+          return 1
+      }
+      if (a.nxtchk.getTime() < b.nxtchk.getTime()){
+        return -1
+      }
+      return 0
+    })
+  );
+}, 15000);
+
 module.exports = (function(){
   return{
   }
